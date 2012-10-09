0c959b5118ac109c00b2ffc3072649e2abfaa799
Gilad Zohari <gilad@ftbpro.com>
2012-10-09 18:04:38 +0200

    fix require

--- lib/visit_counter_updater.rb
+++ lib/visit_counter_updater.rb
@@ -1,7 +1,5 @@
 require "visit_counter_updater/version"
 require "visit_counter_updater/key"
 require "visit_counter_updater/visit_counter_updater"
-require "visit_counter_updater/store/abstract_store"
 require "visit_counter_updater/store/redis_store"
-require "visit_counter_updater/store/rails_store"
 require "visit_counter_updater/store"
